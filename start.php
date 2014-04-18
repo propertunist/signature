57d538a97a5e61019be1ce1e637922178bd29705
ura soul <ura@infiniteeureka.com>
2014-04-18 12:50:42 +0100

    Update start.php
    moved change from branch to master

--- start.php
+++ start.php
@@ -5,8 +5,6 @@ elgg_register_event_handler('init', 'system', 'signature_init');
  * Init
  */
 function signature_init() {
-	//register actions
 		elgg_extend_view('css/elgg', 'css/signature');
-		elgg_extend_view('output/elgg','default/output');
-		
+		elgg_extend_view('annotation/generic_comment','output/signature', 501);
 }
