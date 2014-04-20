9a2cfb9fac9912f784c7df0554113ea2e0f8999a
ura soul <ura@infiniteeureka.com>
2014-04-20 11:27:34 +0100

    Update start.php
    added plugin hook to validate the signature text that is added via the user settings tool page.

--- start.php
+++ start.php
@@ -1,12 +1,38 @@
 <?php
-elgg_register_event_handler('init', 'system', 'signature_init');
+
+/*****
+ * plugin hook to be run when the signature is changed via the tool settings page
+ ******/
+ 
+function validate_signature($hook, $type, $value, $params)
+{
+    $plugin = $params['plugin'];
+
+    if ($plugin->getID() == 'signature') // check if the signature plugin called this hook
+    {
+        if ($params['name'] == 'signature') // check if the signature variable is the one being changed by this hook
+        {
+            $value = strip_tags($value,"<br><a><b><strong><i><italic>"); // remove html tags from the signature, except the ones we want to keep
+            return $value; // return the contents of the signature text box
+        }
+        else
+        return null;
+    }
+    else
+        return null;
+}
 
 /**
- * Init
+ * signature initialisation function
  */
 function signature_init() {
-	//register actions
-		elgg_extend_view('css/elgg', 'css/signature');
-		elgg_extend_view('annotation/generic_comment','output/signature', 501);
-		
+	//register hooks
+	elgg_register_plugin_hook_handler('usersetting', 'plugin', 'validate_signature'); // validate the input that is entered on the settings tool page
+    //extend views
+	elgg_extend_view('css/elgg', 'css/signature'); // add css styling to the main elgg css file
+	elgg_extend_view('annotation/generic_comment','output/signature', 501); // place the signature view at the bottom of the generic comment view
 }
+
+elgg_register_event_handler('init', 'system', 'signature_init');
+
+?>
