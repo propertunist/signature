b32fbd4d357e13c9a3ceebc06a8dad6a4877667f
ura soul <ura@infiniteeureka.com>
2014-04-20 11:29:36 +0100

    Update signature.php
    do not output signature on comments that are part of an activity list or widget
    + grab signature from tool settngs instead of profile_manager

--- views/default/output/signature.php
+++ views/default/output/signature.php
@@ -1,16 +1,10 @@
 <?php
-
 // outputs the signature that has been created via profile_manager for the creator of the current comment
 
- 
+$signature = elgg_get_plugin_user_setting('signature',$commenter->getGUID(), 'signature');
 
-
-
-if (($commenter->signature != ''))
-
-  $output = '<div class="elgg-signature">' . $commenter->signature . '</div>'; 
-
- 
+if (($signature != '')&&(!elgg_in_context('activity'))&&(!elgg_in_context('widgets')))
+  $output = '<div class="elgg-signature">' . $signature . '</div>'; 
 
 echo $output;
-
+?>
