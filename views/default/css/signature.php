c2b08e1bb52a2d0b9beb848cec8c849e466e652c
ura soul <ura@infiniteeureka.com>
2014-04-20 11:28:30 +0100

    Update signature.php
    changed padding

--- views/default/css/signature.php
+++ views/default/css/signature.php
@@ -1,3 +1,1 @@
-<?php
-.elgg-signature{font-size:80%; border-top:1px solid #555; padding: 5px 0;}
-?>
+.elgg-signature{font-size:80%; border-top:1px solid rgb(60, 60, 60); padding: 5px 0 0px 5px; margin-top:5px;text-align:left;}
