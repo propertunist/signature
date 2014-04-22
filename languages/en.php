c33fc4975454facf6c78ffeee1d3993df6dc40d1
ura soul <ura@mint>
2014-04-22 19:28:24 +0100

    v.1.0.1 - only show on non-full entity views

--- languages/en.php
+++ languages/en.php
@@ -2,10 +2,11 @@
 /**
  * signature English language file
  */
-
+$tags = htmlspecialchars("<br>, <a>, <b>, <strong>, <i> & <italic>",ENT_QUOTES, "UTF-8");
 $english = array(
 	'signature:title' => 'Signature',
-	'signature:user:config' => 'Here you can enter a message to use as your signature, which will appear at the bottom of the comments that you write.<br/><b>note: </b>all html tags will be removed from the signature after being saved - except: <i>' . htmlentities("<br>, <a>, <b>, <strong>, <i> & <italic>") . '</i>.',
+	'signature:user:config' => 'Here you can enter a message to use as your signature, which will appear at the bottom of the comments that you write.<br/><b>note: </b>all html tags will be removed from the signature after being saved - except: <i>' . $tags . '</i>.',
 );
 					
 add_translation("en", $english);
+?>
--- views/default/output/signature.php
+++ views/default/output/signature.php
@@ -2,7 +2,7 @@
 // outputs the signature that has been created via profile_manager for the creator of the current comment
 $signature = elgg_get_plugin_user_setting('signature',$commenter->getGUID(), 'signature');
 
-if (($signature != '')&&(!elgg_in_context('activity'))&&(!elgg_in_context('widgets')))
+if (($signature != '')&&(($vars['full_view'])))
   $output = '<div class="elgg-signature">' . $signature . '</div>'; 
 
 echo $output;
